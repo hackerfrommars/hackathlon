e5ab31c81aa6911f298bbec38066dfe67ca4bc4b
Yerbolat <sarah.hiyandao@gmail.com>
2018-03-17 15:57:40 +0600

    MLPRegressor

--- task2.py
+++ task2.py
@@ -17,7 +17,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import svm
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.grid_search import GridSearchCV
 
@@ -75,7 +75,7 @@ def heatmap(fulldata,figsize=(25,25),annot_size = 8,cmap=sns.cubehelix_palette(s
 # data = data.drop(data.columns[[3]], 1) # removing Atyrau because correlation is very low -0.24
 
 data1 = data[['Almaty', 'Kyzylorda', 'Atyrau']].values
-data_kyz = data['Kyzylorda'].values
+data_aty = data['Atyrau'].values
 data_alm = data['Almaty'].values
 label = data['Gas_Flow'].values
 # plt.subplot(211)
@@ -84,32 +84,37 @@ label = data['Gas_Flow'].values
 # plt.scatter(data_kyz, label, c='blue', alpha=0.58)
 # plt.show()
 
-# data_kyz = data_kyz.reshape(-1, 1)
+data_aty = data_aty.reshape(-1, 1)
 data_alm = data_alm.reshape(-1, 1)
-# scaler = StandardScaler()
-# data1 = scaler.fit_transform(data1)
+scaler = StandardScaler()
+data_alm = scaler.fit_transform(data_alm)
 
 x_train, x_test, y_train, y_test = train_test_split(data_alm, label, test_size=0.2, random_state=42)
 
-
-log_reg = LogisticRegression()
-log_reg.fit(x_train, y_train)
-predicted = regr.predict(x_test)
+nn = MLPRegressor(activation='relu', )
+n = nn.fit(x_train, y_train)
+predicted = nn.predict(x_test)
 # print('regression coef: ', log_reg.coef_)
 print("mean squeared error: ", mean_squared_error(y_test, predicted))
+
+
+
+# log_reg = LogisticRegression(C=1e5)
+# log_reg.fit(x_train, y_train)
+# predicted = log_reg.predict(x_test)
+# # print('regression coef: ', log_reg.coef_)
+# print("mean squeared error: ", mean_squared_error(y_test, predicted))
 
 # svm_reg = SVR(kernel='rbf', C=1.00)
 # svm_reg.fit(x_train, y_train)
 # predicted = svm_reg.predict(x_test)
-# print ("accur sc: ",accuracy_score(y_test, predicted))
-# # print(average_precision_score(y_test, test_data))
-# print ("class_rep: ", classification_report(y_test, predicted))
+# print("mean squeared error: ", mean_squared_error(y_test, predicted))
 
 
 # rand_for = RandomForestRegressor()
 # rand_for.fit(x_train, y_train)
 # print(rand_for.feature_importances_) # feature importances by cities
-
+#
 # predicted = rand_for.predict(x_test)
 # print("mean squeared error: ", mean_squared_error(y_test, predicted))
 
@@ -119,61 +124,3 @@ print("mean squeared error: ", mean_squared_error(y_test, predicted))
 # predicted = regr.predict(x_test)
 # print('regression coef: ', regr.coef_)
 # print("mean squeared error: ", mean_squared_error(y_test, predicted))
-
-
-
-# crange=list(range(1//10, 2, 0.1))
-# acc_score=[]
-# for c in crange:
-#     svc = svm.SVC(kernel = 'linear',  C=c)
-#     scores = cross_val_score(svc, data, label, cv=10, scoring='accuracy')
-#     acc_score.append(scores.mean())
-# print(sum(acc_score) / float(len(acc_score)))
-
-
-# svm_cl = svm.SVC(kernel='linear', C=)
-# svm_cl.fit(x_train, y_train)
-# predicted = svm_cl.predict(x_test)
-# print ("accur sc: ",accuracy_score(y_test, predicted))
-# # print(average_precision_score(y_test, test_data))
-# print ("class_rep: ", classification_report(y_test, predicted))
-
-
-## presumably 3 is better
-# wcss = []
-# for i in range(1, 11):
-#     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=200, random_state=0)
-#     kmeans.fit(data)
-#     wcss.append(kmeans.inertia_)
-#
-# plt.plot(wcss)
-# plt.xticks(range(1, 11))
-# plt.title('The elbow method')
-# plt.xlabel('Number of clusters')
-# plt.ylabel('wcss score')
-# plt.show()
-
-
-
-# kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=100, random_state=0)
-# y_means = kmeans.fit_predict(data)
-
-
-##AgglomerativeClustering
-# kmeans = AgglomerativeClustering(n_clusters=3, linkage='average', affinity='cosine')
-# y_means = kmeans.fit_predict(data)
-# # print(y_means)
-# unique, counts = np.unique(y_means, return_counts=True)
-# print(dict(zip(unique, counts)))
-# print ("accur sc (agglomerative): ",accuracy_score(label, y_means))
-
-
-
-# plt.scatter(data[y_means==0, 1], data[y_means==0, 2], s=50, c='red', label='Cluster1')
-# plt.scatter(data[y_means==1, 1], data[y_means==1, 2], s=50, c='blue', label='Cluster2')
-# plt.scatter(data[y_means==2, 1], data[y_means==2, 2], s=50, c='cyan', label='Cluster3')
-#
-# # plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=70, c='yellow', label='centroids')
-# plt.title('clusters of materials')
-# plt.legend()
-# plt.show()
